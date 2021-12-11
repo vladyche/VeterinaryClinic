510776a429023d0f571360d8bc399366dc3f6443
Vladyslav Mishchenko <mishchenko.online@gmail.com>
2021-12-12 01:04:36 +0200

    add Owner entity

--- clinic/src/main/java/com/veterinary/clinic/entity/Owner.java
+++ clinic/src/main/java/com/veterinary/clinic/entity/Owner.java
@@ -1,5 +1,61 @@
 package com.veterinary.clinic.entity;
 
-public class Owner {
+import javax.persistence.*;
 
+@Entity
+@Table(name = "owner")
+public class Owner {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "firstname")
+    private String firstname;
+
+    @Column(name = "lastname")
+    private String lastname;
+
+    @Column(name = "patronymic")
+    private String patronymic;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phone")
+    private String phone;
+
+    public Owner(){}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
 }
--- clinic/src/test/java/com/veterinary/clinic/entity/OwnerTest.java
+++ clinic/src/test/java/com/veterinary/clinic/entity/OwnerTest.java
@@ -1,5 +1,25 @@
 package com.veterinary.clinic.entity;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.junit.jupiter.api.Test;
+
 public class OwnerTest {
 
+    @Test
+    void owner(){
+        SessionFactory sessionFactory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Owner.class)
+                .buildSessionFactory();
+
+        try (sessionFactory; Session session = sessionFactory.getCurrentSession()) {
+            session.beginTransaction();
+
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
