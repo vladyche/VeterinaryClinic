9013637571aa170af3f459110c12f4fba97b2c76
Vladyslav Mishchenko <mishchenko.online@gmail.com>
2021-12-14 00:49:40 +0200

    Owner DAO interface

---
+++ clinic/src/main/java/com/veterinary/clinic/dao/OwnerDao.java
@@ -0,0 +1,13 @@
+package com.veterinary.clinic.dao;
+
+import com.veterinary.clinic.entity.Owner;
+
+import java.util.List;
+
+public interface OwnerDao {
+    Owner create(Owner owner);
+    Owner findByPhone(String phoneNumber);
+    Owner update(Owner owner);
+    String delete(long id);
+    List<Owner> findAll();
+}
