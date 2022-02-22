c662ea213fd4570504882d7a64bfd7be4388c905
Vladyslav Mishchenko <mishchenko.online@gmail.com>
2022-02-22 02:43:25 +0200

    Thymeleaf config

--- clinic/src/main/java/com/veterinary/clinic/config/WebMvcConfig.java
+++ clinic/src/main/java/com/veterinary/clinic/config/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package com.veterinary.clinic.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
